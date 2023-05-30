4bcf9ff338d27e611bfd17ccdca8fbfb9be2508f
Valithor Obsidion <valzargaming@gmail.com>
2023-05-30 03:21:37 -0400

    Notes

--- main.php
+++ main.php
@@ -70,80 +70,81 @@ $ingredient5 = new Ingredient(
 );*/
 
 /* Fruity Tomato Soup
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Hylian Tomato"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Fresh Milk"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Rock Salt"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Hylian Tomato'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Fresh Milk'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Rock Salt'));
 */
 
 /* Veggie Cream Soup
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Fortified Pumpkin"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Rock Salt"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Fresh Milk"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Fresh Milk"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Raw Bird Thigh"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Fortified Pumpkin'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Rock Salt'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Fresh Milk'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Fresh Milk'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Raw Bird Thigh'));
 */
 
 
 /* Monster Curry
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Hylian Rice"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Goron Spice"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Monster Extract"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Hylian Rice'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Goron Spice'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Monster Extract'));
 */
 
 /* Carrot Stew
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Endura Carrot"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Goat Butter"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Fresh Milk"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Tabantha Wheat"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Fairy"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Endura Carrot'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Goat Butter'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Fresh Milk'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Tabantha Wheat'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Fairy'));
 */
 
 
 /* Salmon Meunière
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Hearty Salmon"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Goat Butter"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Tabantha Wheat"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Fairy"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Endura Carrot"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Hearty Salmon'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Goat Butter'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Tabantha Wheat'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Fairy'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Endura Carrot'));
 */
 
 /*Salmon Risotto
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Razorclaw Crab"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Hylian Rice"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Rock Salt"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Goat Butter"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Hearty Salmon"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Razorclaw Crab'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Hylian Rice'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Rock Salt'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Goat Butter'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Hearty Salmon'));
 */
 
 /* Crab Risotto
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Hyrule Bass"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Bright-Eyed Crab"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Rock Salt"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Hylian Rice"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Goat Butter"));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Hyrule Bass'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Bright-Eyed Crab'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Rock Salt'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Hylian Rice'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Goat Butter'));
 */
 
 /* Seafood Paella
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Armored Porgy"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Razorclaw Crab"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Hylian Rice"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Rock Salt"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Goat Butter"));
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Armored Porgy'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Razorclaw Crab'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Hylian Rice'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Rock Salt'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Goat Butter'));
 */
 
 /* Seafood Rice Balls (Test failed, last in list) (Patched by reversing array if no recipe containing generic categories were found in the highest ordered recipes)
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Hyrule Bass"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Sneaky River Snail"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Rock Salt"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Hylian Rice"));
-$ingredient5 = new Ingredient($materials_collection->get('Euen name', "Goat Butter"));
-*/
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Hyrule Bass'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Sneaky River Snail'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Rock Salt'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Hylian Rice'));
+$ingredient5 = new Ingredient($materials_collection->get('Euen name', 'Goat Butter'));
+//
 
-// Fruitcake
-$ingredient1 = new Ingredient($materials_collection->get('Euen name', "Apple"));
-$ingredient2 = new Ingredient($materials_collection->get('Euen name', "Wildberry"));
-$ingredient3 = new Ingredient($materials_collection->get('Euen name', "Cane Sugar"));
-$ingredient4 = new Ingredient($materials_collection->get('Euen name', "Tabantha Wheat"));
+/* Fruitcake (Test failed, Array to string conversion) (Patched by adding 'continue 2' inside of 'foreach ($opt as $o)' for parsed['optional']
+$ingredient1 = new Ingredient($materials_collection->get('Euen name', 'Apple'));
+$ingredient2 = new Ingredient($materials_collection->get('Euen name', 'Wildberry'));
+$ingredient3 = new Ingredient($materials_collection->get('Euen name', 'Cane Sugar'));
+$ingredient4 = new Ingredient($materials_collection->get('Euen name', 'Tabantha Wheat'));
+*/
 
 $ingredients = [$ingredient1 ?? NULL, $ingredient2 ?? NULL, $ingredient3 ?? NULL, $ingredient4 ?? NULL, $ingredient5 ?? NULL];
 var_dump('[INGREDIENTS]', $ingredients);
